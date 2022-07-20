b4821c116ca86e120dff3df63a0813dfe2bbf406
JVignesh4 <vigneshbuji04@gmail.com>
2022-07-20 12:11:48 +0530

    Added: Validate password Function With Atleast One Digit

--- UserDetailsRegexPattern.java
+++ UserDetailsRegexPattern.java
@@ -5,4 +5,5 @@ public class UserDetailsRegexPattern {
     static String EMAIL = "^([a-z0-9]+([-$%&+.]?[0-9a-z]+))[@][a-z0-9]+[.][a-z]{3,}([.][a-z]{2,})?$";
     static String PHONE_NUMBER = "^([1-9]+[0-9]+)[\\s][0-9]{10}$";
     static String PASSWORD = "^(?=.*[a-z])(?=.*[A-Z])(?!.+[0-9]).{8,}$";
+
 }
